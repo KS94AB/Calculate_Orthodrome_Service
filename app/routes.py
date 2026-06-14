a240e6e199899424fd3abe30f7972b797d64cd05
Марсель Марганов <marganovmarsel@gmail.com>
2026-06-14 14:17:02 +0300

    parameters from url

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,16 @@ def index():
 
 @main.route("/orthodromy")
 def orthodromy():
-    return " "
+    start_lon = request.args.get("start_lon")
+    start_lat = request.args.get("start_lat")
+    end_lon = request.args.get("end_lon")
+    end_lat = request.args.get("end_lat")
+    nodes_count = request.args.get("nodes_count")
+
+    return jsonify({
+        "start_lon" : start_lon,
+        "start_lat" : start_lat,
+        "end_lon" : end_lon,
+        "end_lat" : end_lat,
+        "nodes_count" : nodes_count
+    })
